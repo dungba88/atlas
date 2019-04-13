7cd8dbaf6a90e8237ded389db806104b0a3c6336
Anh Dung Bui <dungba.sg@gmail.com>
2019-04-13 14:53:55 +0700

    refactor

--- src/test/java/org/joo/atlas/test/AtlasTest.java
+++ src/test/java/org/joo/atlas/test/AtlasTest.java
@@ -1,6 +1,7 @@
 package org.joo.atlas.test;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.joo.atlas.models.Batch;
 import org.joo.atlas.models.Task;
@@ -8,10 +9,13 @@ import org.joo.atlas.tasks.impl.DAGTaskSorter;
 import org.joo.atlas.tasks.impl.DefaultTaskMapper;
 import org.joo.atlas.tasks.impl.DefaultTaskSubmitter;
 import org.joo.atlas.tasks.impl.PooledTaskRunner;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class AtlasTest {
 
-    public static void main(String[] args) throws InterruptedException {
+    @Test
+    public void testGraph() throws InterruptedException {
         var taskMapper = new DefaultTaskMapper().with("test-task", PrintTaskJob::new);
         var submitter = new DefaultTaskSubmitter(new DAGTaskSorter(), new PooledTaskRunner(16, 2), taskMapper);
         var batch = Batch.of("test", //
@@ -23,14 +27,16 @@ public class AtlasTest {
                 Task.of("7", "task7", "test-task", new String[] { "2" }, 100L), //
                 Task.of("6", "task6", "test-task", new String[0], 100L), //
                 Task.of("8", "task8", "test-task", new String[0], 500L));
+        var exRef = new AtomicReference<Throwable>();
         var latch = new CountDownLatch(1);
-        var promise = submitter.submitTasks(batch);
-        promise.always((s, r, e) -> {
-            if (e != null)
-                e.printStackTrace();
-            latch.countDown();
-        });
+        submitter.submitTasks(batch) //
+                 .always((s, r, e) -> {
+                     if (e != null)
+                         exRef.set(e);
+                     latch.countDown();
+                 });
         latch.await();
+        Assert.assertNull(exRef.get());
         submitter.stop();
     }
 }
